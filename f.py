091fa75afddac6c8e66c766e2dfc55e5b7ba2fbb
Ben Po <benjx21@gmail.com>
2021-04-28 14:08:25 -0500

    new new files

---
+++ f.py
@@ -0,0 +1,5 @@
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+name = f'Hello, {full_name.title()}!'
+print(name)
---
+++ personal_message.py
@@ -0,0 +1,16 @@
+first_name = 'Ben'
+print(first_name.upper())
+print('Hello, ', first_name.upper(), 'how are you?')
+
+first_name1 = "albert einstine"
+message = "insert some famous albert einstien quote"
+print(first_name1, "said", message)
+
+name1 = " jackdorsey"
+name2 = "jackdorsey "
+name3 = " jackdorsey "
+
+print(name1, tname2, name3)
+print(name1.lstrip())
+print(name2.rstrip())
+print(name3.strip())
